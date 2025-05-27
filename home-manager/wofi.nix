6b62f333d2062936f42627cbacab36bfde19beab
Brynley Llewellyn-Roux <git.escalator677@passmail.com>
2025-05-27 21:43:20 +1000

    fix: minor bug fixes

--- home-manager/wofi.nix
+++ home-manager/wofi.nix
@@ -4,6 +4,8 @@
     settings = {
       show = "drun";
       matching = "fuzzy";
+      allow_images = true;
+      prompt = "search";
     };
     style = ''
       * {
@@ -18,6 +20,25 @@
       }
       #outer-box {
         padding: 12px;
+      }
+      #input {
+        padding: 12px;
+        border: 1px solid #35363e;
+        border-radius: 12px;
+        margin-bottom: 12px;
+      }
+      #input > * {
+        margin-right: 8px;
+      }
+      #entry {
+        padding: 12px;
+        border-radius: 12px;
+      }
+      #entry:selected {
+        background-color: rgba(30, 46, 46, 1.0);
+      }
+      #img {
+        margin-right: 8px;
       }
     '';
   };
