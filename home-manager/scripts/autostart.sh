d3d75780da68497de997eb0518e459c96fe78cfa
Brynley Llewellyn-Roux <blynblack@gmail.com>
2025-05-29 13:50:31 +1000

    fix: temporary app startup fix

---
+++ home-manager/scripts/autostart.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Wait for the Wayland socket to appear
+while [ ! -S "$XDG_RUNTIME_DIR/wayland-1" ]; do sleep 0.1; done
+
+# Load OpenRGB profile and minimize
+openrgb --profile Default --startminimized &
+
+# Set cursor theme and size
+hyprctl setcursor capitaine-cursors 24
+
+# Restart user services
+systemctl --user restart \
+  hyprpaper.service \
+  waybar.service \
+  blueman-applet.service \
+  kdeconnect.service \
+  kdeconnect-indicator.service
